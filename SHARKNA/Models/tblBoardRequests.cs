bca7a9887f1c75cc101cc3bd178ae7edcd97b29c
Komail262 <kmool@hotmail.com>
2024-07-21 10:54:12 +0300

    test

--- SHARKNA/Models/tblBoardRequests.cs
+++ SHARKNA/Models/tblBoardRequests.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; }
         
-        public string UserName { get; set; }
+        public string UserName { get; set; } //test
         public string Email { get; set; }
         public string MobileNumber { get; set; }
         public string FullNameAr { get; set; }
