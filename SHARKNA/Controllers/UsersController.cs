5b20772b51276b58504f4bf18357116bf2fbb87e
Komail262 <kmool@hotmail.com>
2024-07-23 14:26:55 +0300

    last

---
+++ SHARKNA/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SHARKNA.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using SHARKNA.Domain;
+
+namespace SHARKNA.Controllers
+{
+    public class UsersController : Controller
+    {
+        
+        private readonly UserDomain _User;
+
+        public UsersController( UserDomain User)
+        {
+           
+            _User = User;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _User.GettblUsers(); 
+            return View(users);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- SHARKNA/Domain/UserDomain.cs
+++ SHARKNA/Domain/UserDomain.cs
@@ -1,6 +1,23 @@
-﻿namespace SHARKNA.Domain
+﻿using System.Collections.Generic;
+using System.Linq;
+using SHARKNA.Models; 
+
+namespace SHARKNA.Domain
 {
     public class UserDomain
     {
+        private readonly SHARKNAContext _context;
+
+
+        public UserDomain(SHARKNAContext context)
+        {
+            _context = context;
+        }
+
+        
+        public IEnumerable<tblUsers> GettblUsers()
+        {
+            return _context.tblUsers.ToList();
+        }
     }
 }
