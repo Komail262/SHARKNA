1eef62a2996c6dc016731c3473aabc870acce4fc
220008386 <220008386@student.kfu.edu.sa>
2024-07-24 11:41:19 +0300

    last Updeat

--- SHARKNA/Controllers/UsersController.cs
+++ SHARKNA/Controllers/UsersController.cs
@@ -1,30 +1,71 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SHARKNA.Models;
+using SHARKNA.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Extensions.Logging;
 using System.Diagnostics;
-using SHARKNA.Domain;
 
 namespace SHARKNA.Controllers
 {
     public class UsersController : Controller
     {
-        
-        private readonly UserDomain _User;
+        private readonly UserDomain _userDomain;
 
-        public UsersController( UserDomain User)
+        public UsersController(UserDomain userDomain)
         {
-           
-            _User = User;
+            _userDomain = userDomain;
         }
 
         public IActionResult Index()
         {
-            var users = _User.GettblUsers(); 
+            var users = _userDomain.GetTblUsers();
             return View(users);
         }
 
+        // ميثود لعرض النموذج لإدخال مستخدم جديد
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // ميثود لمعالجة بيانات النموذج وإدخالها في قاعدة البيانات
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(tblUsers user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.Id = Guid.NewGuid(); // تعيين معرف جديد
+                _userDomain.AddUser(user); // استخدام UserDomain لإضافة المستخدم
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // ميثود لعرض صفحة التحرير
+        public IActionResult Edit(Guid id)
+        {
+            var user = _userDomain.GetTblUserById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // ميثود لمعالجة بيانات التحرير وتحديثها في قاعدة البيانات
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(tblUsers user)
+        {
+            if (ModelState.IsValid)
+            {
+                _userDomain.UpdateUser(user);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
 
         public IActionResult Privacy()
         {
--- SHARKNA/Domain/UserDomain.cs
+++ SHARKNA/Domain/UserDomain.cs
@@ -1,23 +1,37 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using SHARKNA.Models; 
+using SHARKNA.Models;
 
 namespace SHARKNA.Domain
 {
     public class UserDomain
     {
         private readonly SHARKNAContext _context;
-
-
         public UserDomain(SHARKNAContext context)
         {
             _context = context;
         }
 
-        
-        public IEnumerable<tblUsers> GettblUsers()
+        public IEnumerable<tblUsers> GetTblUsers()
         {
             return _context.tblUsers.ToList();
+        }
+
+        public tblUsers GetTblUserById(Guid id)
+        {
+            return _context.tblUsers.FirstOrDefault(u => u.Id == id);
+        }
+
+        public void AddUser(tblUsers user)
+        {
+            _context.tblUsers.Add(user);
+            _context.SaveChanges();
+        }
+
+        public void UpdateUser(tblUsers user)
+        {
+            _context.tblUsers.Update(user);
+            _context.SaveChanges();
         }
     }
 }
