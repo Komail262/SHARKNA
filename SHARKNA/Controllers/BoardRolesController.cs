c00e7005304eebe499368e648db3b9de8200216d
Alomran <hsujec9@gmail.com>
2024-08-08 11:13:22 +0300

    boardroles done

---
+++ SHARKNA/Controllers/BoardRolesController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SHARKNA.Models;
+using SHARKNA.Domain;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using SHARKNA.ViewModels;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+namespace SHARKNA.Controllers
+{
+    public class BoardRolesController : Controller
+    {
+        private readonly BoardRolesDomain _BoardRolesDomain;
+
+        public BoardRolesController(BoardRolesDomain boardRoles)
+        {
+            _BoardRolesDomain = boardRoles;
+        }
+
+        public IActionResult Index(string Successful = "", string Falied = "")
+        {
+            if (Successful != "")
+                ViewData["Successful"] = Successful;
+            else if (Falied != "")
+                ViewData["Falied"] = Falied;
+
+            var boardRoles = _BoardRolesDomain.GettbBoardRoles();
+            return View(boardRoles);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(BoardRolesViewModel boardRoles)
+        {
+            if (ModelState.IsValid)
+            {
+               boardRoles.Id = Guid.NewGuid();
+                _BoardRolesDomain.AddBoardRoles(boardRoles);
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+
+        public IActionResult Edit(Guid id)
+        {
+
+            var boardRoles = _BoardRolesDomain.GetTblBoardRolesById(id);
+            if (boardRoles == null)
+            {
+                return NotFound();
+            }
+            return View(boardRoles);
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(BoardRolesViewModel boardRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                _BoardRolesDomain.UpdateBoardRoles(boardRoles);
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(boardRoles);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Guid id)
+        {
+            string Successful = "";
+            string Falied = "";
+            try
+            {
+
+
+                int check = _BoardRolesDomain.DeleteBoaRoles(id);
+                if (check == 1)
+                {
+                    Successful = "تم حذف  بنجاح";
+                }
+
+                else
+                {
+                    Falied = "حدث خطأ";
+
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Falied = "حدث خطأ";
+
+            }
+            //_boardDomain.DeleteBoard(id);
+            return RedirectToAction(nameof(Index), new { Successful = Successful, Falied = Falied });
+
+
+        }
+
+
+    }
+}
+
+
---
+++ SHARKNA/ViewModels/BoardRolesViewModel.cs
@@ -0,0 +1,36 @@
+﻿using SHARKNA.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+
+
+
+namespace SHARKNA.ViewModels
+{
+    public class BoardRolesViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "هذا الحقل اجباري")]
+        [StringLength(100)]
+        [DisplayName("الاسم العربي")]
+        
+        public string NameAr { get; set; }
+
+        [Required(ErrorMessage = "هذا الحقل اجباري")]
+        [StringLength(100)]
+        [DisplayName("الاسم بالانجليزي")]
+        public string NameEn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public bool IsActive { get; set; }
+
+
+
+
+
+    }
+}
+
+
