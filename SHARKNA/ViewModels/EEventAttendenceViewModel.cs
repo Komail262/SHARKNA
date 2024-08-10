648460d698e91eb22be184633f81a1c3b81eb6ef
AlrubayyiAhmed <wdt7222@gmail.com>
2024-08-10 22:22:59 +0300

    eventattendence

---
+++ SHARKNA/ViewModels/EEventAttendenceViewModel.cs
@@ -0,0 +1,42 @@
+﻿using SHARKNA.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace SHARKNA.ViewModels
+{
+    public class EEventAttendenceViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime, ErrorMessage = "هذا الحقل اجباري")]
+        [DisplayName("تاريخ الحدث")]
+        public DateTime EventDate { get; set; }
+
+
+        [Required(ErrorMessage = "هذا الحقل اجباري")]
+        [DisplayName("عدد أيام الفعالية")]
+        public int Day { get; set; }
+
+
+        [Required(ErrorMessage = "هذا الحقل اجباري")]
+        [DisplayName("اسم الفعالية")]
+        public Guid EventstId { get; set; }
+
+        public string EventName { get; set; }  //هنا عشان يعرض اسم الفعالية
+
+
+
+        [Required(ErrorMessage = "هذا الحقل اجباري")]
+        [DisplayName("اسم الأعضاء بالفعالية")]
+        public Guid EventMemId { get; set; }
+
+        public string EventMemName { get; set; }  //هنا عشان يعرض اسم الأعضاء بالفعالية 
+
+
+
+
+        public bool IsAttend { get; set; }
+
+    }
+}
